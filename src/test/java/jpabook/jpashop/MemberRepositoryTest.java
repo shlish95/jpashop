700e4d1d5ecc242cf0afa847b3e5d546f00dfd46
shlihs95 <fhzptvh@gmail.com>
2024-01-15 07:20:07 +0900

    4. 회원 도메인 개발_회원 서비스 개발

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,39 +1,39 @@
-package jpabook.jpashop;
-
-import jpabook.jpashop.domain.Member;
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class MemberRepositoryTest {
-
-    @Autowired MemberRepository memberRepository;
-
-    @Test
-    @Transactional //테스트의 경우 자동으로 롤백해줌
-    @Rollback(false) //자동 롤백을 막아줌
-    public void testMember() throws Exception {
-        //ginve
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        //when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-
-        Assertions.assertThat(findMember).isEqualTo(member);
-        //같은 트랜잭션 안에서 저장하고 조회하면 영속성 컨텍스트가 같다.
-        //같은 영속성 컨텍스트에서 식별자가 같으면 경우 같은 엔티티로 인식
-    }
-}
+//package jpabook.jpashop;
+//
+//import jpabook.jpashop.domain.Member;
+//import org.assertj.core.api.Assertions;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class MemberRepositoryTest {
+//
+//    @Autowired MemberRepository memberRepository;
+//
+//    @Test
+//    @Transactional //테스트의 경우 자동으로 롤백해줌
+//    @Rollback(false) //자동 롤백을 막아줌
+//    public void testMember() throws Exception {
+//        //ginve
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        //when
+//        Long savedId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(savedId);
+//
+//        //then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//
+//        Assertions.assertThat(findMember).isEqualTo(member);
+//        //같은 트랜잭션 안에서 저장하고 조회하면 영속성 컨텍스트가 같다.
+//        //같은 영속성 컨텍스트에서 식별자가 같으면 경우 같은 엔티티로 인식
+//    }
+//}
